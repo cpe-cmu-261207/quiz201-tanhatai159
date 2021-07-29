0c2c19010c5d5ed850b1f542b8f67c895865d2de
salaza <gun159@outlook.com>
2021-07-29 14:00:36 +0700

    test

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const reset_btn = document.querySelector("#reset")
+const p = document.querySelector("#id")
 
 // define more constants and variables here
 

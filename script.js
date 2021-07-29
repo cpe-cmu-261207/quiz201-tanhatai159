093ed0af87347c226d26aa27ec934889bf1f6555
salaza <gun159@outlook.com>
2021-07-29 14:10:18 +0700

    test2

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 const reset_btn = document.querySelector("#reset")
-const p = document.querySelector("#id")
+const p = document.querySelector("#text")
+const search_btn = document.querySelector("#search")
+const oringin_word = p.innerHTML;
+const color = document.querySelector("#color")
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+
 }
+
+reset_btn.addEventListener("click", () => {
+  p.innerHTML = oringin_word;
+})
+
+search_btn.addEventListener("click", () => {
+  
+})
+
 
 // more codes for Search and Reset buttons here
